b809941913a2bfec52ffe9519e55cd8edbfb9cb6
unknown <liorwaknin@gmail.com>
2018-04-25 20:24:05 +0300

    #15 (issue number) Done, and complete admin-login page

--- AgudaApp/src/pages/admin-login/admin-login.ts
+++ AgudaApp/src/pages/admin-login/admin-login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController, Loading } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, Loading, AlertController, ToastController } from 'ionic-angular';
 import { AdminPanelPage } from '../admin-panel/admin-panel';
 import { AngularFireAuth } from 'angularfire2/auth';
 
@@ -19,7 +19,9 @@ export class AdminLoginPage {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private afAuth: AngularFireAuth,
-              private loadingCtrl: LoadingController) {
+              private loadingCtrl: LoadingController,
+              private alertCtrl: AlertController,
+              private toastCtrl: ToastController) {
   }
 
   async login() {
@@ -40,19 +42,56 @@ export class AdminLoginPage {
     } catch (e) {
       console.error(e);
       loader.dismiss().then(() => {
-        let load2 = this.loadingCtrl.create({
-          content: e.message,
-          spinner: 'text only',
-          duration: 3000,
-          cssClass: 'customLoad'
+        let alert = this.alertCtrl.create({
+          title: 'Login Failed',
+          message: e.message,
+          buttons: ['OK']
         });
-        load2.present();
-      })
+        alert.present();
+      });
     }
   }
 
   forgotPassword(){
-
+    let alert = this.alertCtrl.create({
+      cssClass: 'alertClass',
+      title: 'Password Recovery',
+      subTitle: 'Enter your Email address',
+      inputs: [
+        {
+          name: 'emailAddress',
+          type: 'email',
+          placeholder: 'Email',
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirm',
+          handler: data => {
+            console.log(data.emailAddress);
+            let toastOpt = {
+              message: '',
+              duration: 3000,
+              cssClass: 'toastClass',
+              position: 'middle'
+            };
+            this.afAuth.auth.sendPasswordResetEmail(data.emailAddress)
+              .then(() => {
+                toastOpt.message = 'Mail with steps about password recovery was sent to you';
+              })
+              .catch(err => {
+                toastOpt.message = err.message;
+              });
+            this.toastCtrl.create(toastOpt).present();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
   ionViewDidLoad() {
