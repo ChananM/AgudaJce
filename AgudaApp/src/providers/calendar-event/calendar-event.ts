b12dbdc9d759ee651cceadf0c89321a3b173b7ac
ChananM <chanancool@gmail.com>
2018-05-01 21:29:05 +0300

    Build commit

--- AgudaApp/src/providers/calendar-event/calendar-event.ts
+++ AgudaApp/src/providers/calendar-event/calendar-event.ts
@@ -17,11 +17,6 @@ export class CalendarEventProvider {
   }
   async load(){
     await this.posts.length > 0;
-    // this.posts = [ // delete this in your time
-    //   new CalendarEvent("",this.isoToDate("2018-04-24"),"Test","האירוע המטריף של הקיץ - בדיקת אפליקציות", false),
-    //   new CalendarEvent("https://s-i.huffpost.com/gen/1842880/images/n-NIGHTCLUB-628x314.jpg",this.isoToDate("2018-03-10"),"מסיבה","בלה-בלה-בלה,לה-לה-לה", false),
-    //   new CalendarEvent("https://c.wallhere.com/photos/90/73/disco_club_dance_hands-723564.jpg!d",this.isoToDate("2018-04-24"),"Test3","Let's test our calendar event", false)
-    // ];
   }
 
   addEvent(event: CalendarEvent){
@@ -30,10 +25,6 @@ export class CalendarEventProvider {
 
   deleteEvent(event: CalendarEvent){
     return this.dbProvider.deleteCalEvent(event);
-  }
-
-  private isoToDate(dateStr:string) : Date{
-    return new Date(dateStr);
   }
 }
 
