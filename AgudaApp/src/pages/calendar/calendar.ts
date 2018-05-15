f470a1e067b5fb1c4cfe34a8520c6a32c83c40cb
Eliyah Weinberg <eliyahweinberg@gmail.com>
2018-05-15 20:03:23 +0300

    platform calendar integration #9

--- AgudaApp/src/pages/calendar/calendar.ts
+++ AgudaApp/src/pages/calendar/calendar.ts
@@ -1,7 +1,9 @@
+import { CalendarEvent } from './../../models/calendarEvent.model';
 import { CalendarEventProvider } from './../../providers/calendar-event/calendar-event';
 import { Component } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Calendar } from '@ionic-native/calendar';
 
 @Component({
   selector: 'page-calendar',
@@ -14,7 +16,8 @@ export class CalendarPage {
   constructor(public navCtrl: NavController,
               public calendarProvider: CalendarEventProvider,
               public platform: Platform,
-              private iab: InAppBrowser
+              private iab: InAppBrowser,
+              private calendar: Calendar
             ) {
   }
 
@@ -34,13 +37,43 @@ export class CalendarPage {
     this.calendarProvider.posts[i].open = !this.calendarProvider.posts[i].open;
   }
 
-  addToPlatformCalendar(i){
-    console.log("this event will be added to the platform calendar");
-    console.log(this.calendarProvider.posts[i]);
+  addToPlatformCalendar(event: CalendarEvent){
+    if (this.platform.is('core')){
+      alert("Adding an event to a calendar aviable on mobile only");
+      return;
+    } 
+    console.log(event.date + "this event will be added to the platform calendar");
+    if (this.platform.is('android')){
+      this.calendar.hasWritePermission().then(permission => {
+        if (!permission){
+          console.log("required write permission");
+          this.calendar.requestReadWritePermission().then(res => {
+          }, err => {
+            console.log('err: perrmission request error', err);
+            alert("perrmission request error");
+          });
+        }
+      }).then(result => {
+        this.calendar.createEventInteractively(event.headline,"",event.content,event.getDate(),event.getDate()).then(res => {
+        });
+      }).catch(failuCallback => {});
+    }
+    else if (this.platform.is('ios')){
+      this.calendar.createEventInteractively(event.headline,"",event.content,event.getDate(),event.getDate()).then(res => {
+      }, err => {
+        console.log('err: adding event error', err);
+        alert("adding event error: " + err);
+      });
+    }
+    
   }
 
   gotoPurachase(url: string){
     console.log("redirect to: " + url);
     /*const browser = */ this.iab.create(url);
+  }
+
+  closeItem(post: CalendarEvent){
+    post.open = false;
   }
 }
